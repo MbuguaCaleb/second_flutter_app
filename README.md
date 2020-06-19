58658a539b75660d21b638aacc75ce7745d0b9d5
MbuguaCaleb <MbuguaCaleb>
2020-06-19 18:54:47 +0300

    'hot-reload-and-stateless-widgets'

--- README.md
+++ README.md
@@ -57,6 +57,41 @@ fonts:
         - asset: fonts/IndieFlower-Regular.ttf
 
 ```
+**Stateless widgets and Hot Reload**
+
+```
+Hot reload is a mechanism that enables you to automatically compile changes and run your application.
+It brings in the concept od stateless widgets
+```
+**Stateless vs Stateful Widgets**
+```
+(a)Stateless Widgets-the  state of the widget cannot change over time.
+example the colors, the layouts which we use over time..The are usually final
+and cannot change overtime as we use the application.
+
+It can contain data but that data cannot change once the widget has been initialized.
+
+(b)Stateful Widgets-It layout,its color or any data inside it can change overtime.
+exmaple some kind of conting widget.where count increments.
+
+N/B
+Whenever we make any slight change of code inside the stateless widget tree, flutter is going to
+detect that and it causes the build function under the stateless widget to re-run updating what
+we see on the screen.
+
+It does not have to rebuild the whole app that we create.It just rebuilds the code inside it.
+
+In future i could have may stateless widgets and when we make a change inside one of those flutter 
+needs only to rebuild that widget and update on the screen.
+
+This causes a much quicker update in the device preview.
+
+Override means that you are going to use the build method of the child class, overiding that of the 
+parent.
+
+(You are going to override the method of a parent class and use that of the child class .
+
+```
 **Notes by**
 
 ```
--- lib/main.dart
+++ lib/main.dart
@@ -5,33 +5,44 @@ import 'package:flutter/material.dart';
 /*It shall help me use google design features inside this application*/
 /*It is like a wrapper for all our other widgets inside this application*/
 void main() => runApp(MaterialApp(
-  home:Scaffold(
-    appBar: AppBar(
-      title: Text('my first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        'Caleb begins on flutter',
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2.0,
-          color: Colors.grey[600],
-          fontFamily: 'indieFlower'
-        ),
+  home:Home()
+));
 
+
+
+/*Custom hot reload flutter class*/
+/*I shall be returning all the widgets whose state does not change under the Stateless Widget*/
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
-    ),
+      body: Center(
+        child: Text(
+          'Caleb begins on flutter',
+          style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'indieFlower'
+          ),
+
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: Text('click'),
         backgroundColor: Colors.red[600],
-      ),
+      )
 
-  )
-));
+    );
+  }
+}
 
 
 
